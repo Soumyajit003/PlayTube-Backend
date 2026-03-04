900232bd1a7a30b88b0ae64c3b9cfd457b93fdcb
Soumyajit003 <soumya.bera003@gmail.com>
2026-03-05 04:00:21 +0530

    Controller: get all videos controller is added

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,3 +1,4 @@
+import mongoose, { isValidObjectId } from "mongoose";
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -25,6 +26,10 @@ const uploadVideo = asyncHandler(async (req, res) => {
   const videoLocalPath = req.files.videofile[0].path;
   const thumbnailLocalPath = req.files.thumbnail[0].path;
 
+  if(!videoLocalPath || !thumbnailLocalPath){
+    throw new ApiError(400, "Video file and thumbnail are required!!!");
+  }
+
   const videoUploaded = await uploadOnCloudinary(videoLocalPath);
   const thumbnailUploaded = await uploadOnCloudinary(thumbnailLocalPath);
 
@@ -49,11 +54,101 @@ const uploadVideo = asyncHandler(async (req, res) => {
 
   return res
     .status(200)
-    .json(new ApiResponse(200, "Video uploaded successfully:)"));
+    .json(new ApiResponse(200,video.title, "Video uploaded successfully:)"));
 });
 
 // controller to get all videos
 const getAllVideos = asyncHandler(async (req, res) => {
+  const { page=1, limit=10, sortBy='createdAt', sortType='desc', query, userId, } = req.query;
+  const pipeline = [];
+
+  // first have to create search index in mongodb atlas
+  // then we have to map fields, like - title, description
+  // so the search text index will find from the fields 
+  // our search index is "search-videos"
+
+  if(query){
+    pipeline.push({
+      $search:{
+        index: "search-videos",
+        text:{
+          query: query,
+          path: ["title", "description"]
+        }
+      }
+    })
+  }
+  
+  // if search using any userid
+  if(userId){
+    if(!isValidObjectId(userId)){
+      throw new ApiError(400, "Invalid userId!!!");
+    }else{
+      pipeline.push({
+        $match:{
+          owner: new mongoose.Types.ObjectId(userId)
+        }
+      })
+    }
+  }
+
+  // fetch only those videos which are published
+  pipeline.push({
+    $match: {
+      isPublished:true
+    }
+  })
+
+  // sortBy - views, createdAt, duration
+  // sortType - ascending, descending
+  if(sortBy && sortType){
+    pipeline.push({
+      $sort:{
+        [sortBy]:sortType == "asc" ? 1 : -1
+      }
+    })
+  }else{
+    pipeline.push({
+      $sort:{
+      createdAt:-1
+      }
+    })
+  }
+
+  // lookup with user
+  pipeline.push(
+    {
+      $lookup:{
+        from:"users",
+        localField: "owner",
+        foreignField: "_id",
+        as:"ownerDetails",
+        pipeline:[{
+          $project:{
+            username:1,
+            "avatar.url":1
+          }
+        }]
+      }
+    },
+    {
+      $unwind:"$ownerDetails"
+    }
+  )
+
+  const videoAggregate = Video.aggregate(pipeline);
+
+  // now its time to do pagination for fast response
+  const options = {
+    page: parseInt(page, 10),
+    limit: parseInt(limit, 10)
+  }
+
+  const video = await Video.aggregatePaginate(videoAggregate, options);
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, video, "Videos fetched successfully"));
 
 })
 
@@ -70,7 +165,7 @@ const getVideoById = asyncHandler( async (req, res) => {
 
     return res
     .status(200)
-    .json(new ApiResponse(200, video, "Video fetched by Id successfully..."));
+    .json(new ApiResponse(200, cloudinaryVideo, "Video fetched by Id successfully..."));
 
 })
 
