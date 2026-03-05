bf80b3e27ff1304cc8394be41444ecca710caeca
Soumyajit003 <soumya.bera003@gmail.com>
2026-03-06 03:33:11 +0530

    getVideoById updated

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -3,7 +3,10 @@ import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { getVideoFromCloudinary, uploadOnCloudinary } from "../utils/cloudinary.js";
+import {
+  getVideoFromCloudinary,
+  uploadOnCloudinary,
+} from "../utils/cloudinary.js";
 import { Video } from "./../models/video.model.js";
 
 // controller for uploading video
@@ -13,20 +16,16 @@ const uploadVideo = asyncHandler(async (req, res) => {
   // validate the data
   // upload the file to the cloudinary using multer
 
-  const { title, description } = req.body;  
+  const { title, description } = req.body;
 
-  if (
-    [title, description,].some(
-      (field) => field?.trim() == ""
-    )
-  ) {
+  if ([title, description].some((field) => field?.trim() == "")) {
     throw new ApiError(400, "All fields are required to upload the video!!!");
   }
 
   const videoLocalPath = req.files.videofile[0].path;
   const thumbnailLocalPath = req.files.thumbnail[0].path;
 
-  if(!videoLocalPath || !thumbnailLocalPath){
+  if (!videoLocalPath || !thumbnailLocalPath) {
     throw new ApiError(400, "Video file and thumbnail are required!!!");
   }
 
@@ -49,124 +48,238 @@ const uploadVideo = asyncHandler(async (req, res) => {
     videoPublicId: videoUploaded.public_id,
     thumbnail: thumbnailUploaded.url,
     thumbnailPublicId: thumbnailUploaded.public_id,
-    owner
+    owner,
   });
 
   return res
     .status(200)
-    .json(new ApiResponse(200,video.title, "Video uploaded successfully:)"));
+    .json(new ApiResponse(200, video.title, "Video uploaded successfully:)"));
 });
 
 // controller to get all videos
 const getAllVideos = asyncHandler(async (req, res) => {
-  const { page=1, limit=10, sortBy='createdAt', sortType='desc', query, userId, } = req.query;
+  const {
+    page = 1,
+    limit = 10,
+    sortBy = "createdAt",
+    sortType = "desc",
+    query,
+    userId,
+  } = req.query;
   const pipeline = [];
 
   // first have to create search index in mongodb atlas
   // then we have to map fields, like - title, description
-  // so the search text index will find from the fields 
+  // so the search text index will find from the fields
   // our search index is "search-videos"
 
-  if(query){
+  if (query) {
     pipeline.push({
-      $search:{
+      $search: {
         index: "search-videos",
-        text:{
+        text: {
           query: query,
-          path: ["title", "description"]
-        }
-      }
-    })
+          path: ["title", "description"],
+        },
+      },
+    });
   }
-  
+
   // if search using any userid
-  if(userId){
-    if(!isValidObjectId(userId)){
+  if (userId) {
+    if (!isValidObjectId(userId)) {
       throw new ApiError(400, "Invalid userId!!!");
-    }else{
+    } else {
       pipeline.push({
-        $match:{
-          owner: new mongoose.Types.ObjectId(userId)
-        }
-      })
+        $match: {
+          owner: new mongoose.Types.ObjectId(userId),
+        },
+      });
     }
   }
 
   // fetch only those videos which are published
   pipeline.push({
     $match: {
-      isPublished:true
-    }
-  })
+      isPublished: true,
+    },
+  });
 
   // sortBy - views, createdAt, duration
   // sortType - ascending, descending
-  if(sortBy && sortType){
+  if (sortBy && sortType) {
     pipeline.push({
-      $sort:{
-        [sortBy]:sortType == "asc" ? 1 : -1
-      }
-    })
-  }else{
+      $sort: {
+        [sortBy]: sortType == "asc" ? 1 : -1,
+      },
+    });
+  } else {
     pipeline.push({
-      $sort:{
-      createdAt:-1
-      }
-    })
+      $sort: {
+        createdAt: -1,
+      },
+    });
   }
 
   // lookup with user
   pipeline.push(
     {
-      $lookup:{
-        from:"users",
+      $lookup: {
+        from: "users",
         localField: "owner",
         foreignField: "_id",
-        as:"ownerDetails",
-        pipeline:[{
-          $project:{
-            username:1,
-            "avatar.url":1
-          }
-        }]
-      }
+        as: "ownerDetails",
+        pipeline: [
+          {
+            $project: {
+              username: 1,
+              "avatar.url": 1,
+            },
+          },
+        ],
+      },
     },
     {
-      $unwind:"$ownerDetails"
+      $unwind: "$ownerDetails",
     }
-  )
+  );
 
   const videoAggregate = Video.aggregate(pipeline);
 
   // now its time to do pagination for fast response
   const options = {
     page: parseInt(page, 10),
-    limit: parseInt(limit, 10)
-  }
+    limit: parseInt(limit, 10),
+  };
 
   const video = await Video.aggregatePaginate(videoAggregate, options);
 
   return res
-  .status(200)
-  .json(new ApiResponse(200, video, "Videos fetched successfully"));
-
-})
+    .status(200)
+    .json(new ApiResponse(200, video, "Videos fetched successfully"));
+});
 
 // controller to get video by Id
-const getVideoById = asyncHandler( async (req, res) => {
-    const { videoId } = req.params;
+const getVideoById = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
 
-    if(!videoId){
-        throw new ApiError(400, "Videos id is missing!!!");
-    }
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Videos id is invalid!!!");
+  }
 
-    const video = await Video.findById(videoId);
-    const cloudinaryVideo = getVideoFromCloudinary(video.videoPublicId);
+  if (!isValidObjectId(req.user?._id)) {
+    throw new ApiError(400, "User id is invalid!!!");
+  }
 
-    return res
+  const video = Video.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "video",
+        as: "likes",
+      },
+    },
+    {
+      $addFields: {
+        likesCount: {
+          $size: "$likes",
+        },
+        isLiked: {
+          $cond: {
+            $if: {
+              $in: [req.user?._id, "$likes.likedBy"],
+            },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $lookup: {
+              from: "subscriptions",
+              localField: "_id",
+              foreignField: "channel",
+              as: "subscribers",
+            },
+          },
+          {
+            $addFields: {
+              subscriberCount: {
+                $size: "$subscribers",
+              },
+              isSubscribed: {
+                $cond: {
+                  if: {
+                    $in: [req.user?._id, "$subscribers.subscriber"],
+                  },
+                  then: true,
+                  else: false,
+                },
+              },
+            },
+          },
+          {
+            $project: {
+              username: 1,
+              "avatar.url": 1,
+              subscriberCount: 1,
+              isSubscribed: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $project: {
+        "videoFile.url": 1,
+        title: 1,
+        description: 1,
+        views: 1,
+        comments: 1,
+        duration: 1,
+        createdAt: 1,
+        owner: 1,
+        isLiked: 1,
+        likesCount: 1,
+      },
+    },
+  ]);
+
+  if (!video) {
+    throw ApiError(500, "Failed to fetch video :(");
+  }
+
+  // increase the views if the video is fetched successfully
+  await Video.findByIdAndUpdate(videoId, {
+    $inc: {
+      views: 1,
+    },
+  });
+
+  // add the video to the watch history of the user
+  await User.findByIdAndUpdate(req.user?._id, {
+    $addToSet: {
+      watchHistory: videoId,
+    },
+  });
+
+  return res
     .status(200)
-    .json(new ApiResponse(200, cloudinaryVideo, "Video fetched by Id successfully..."));
-
-})
+    .json(new ApiResponse(200, video[0], "video details fetched successfully"));
+});
 
 export { uploadVideo, getAllVideos, getVideoById };
